e12835622e64c8f58d7783f8eccf465d0fb4c9dd
rspadi <rspadinger@laposte.net>
2019-02-23 22:09:15 +0100

    FC

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  //bg-dark
+  return (
+    <footer className="color2 text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} CVHub
+    </footer>
+  );
+};
